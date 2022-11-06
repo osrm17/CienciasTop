eb3b003d8f69bc29fb33fe327e93d20d8edb068e
israncho <isragutierrez@ciencias.unam.mx>
2022-11-05 18:15:05 -0600

    doc faltante

--- src/main/java/ctop/model/service/ProductoServiceInterface.java
+++ src/main/java/ctop/model/service/ProductoServiceInterface.java
@@ -2,7 +2,19 @@ package ctop.model.service;
 
 import ctop.model.entity.Producto;
 
+/**
+ * Interfaz que define implementacion que debe tener
+ * un servicio que maneja los productos.
+ * 
+ * @version 1.0
+ */
 public interface ProductoServiceInterface {
-    
+
+    /**
+     * Metodo que devuelve todos los productos disponibles en la
+     * base de datos.
+     * 
+     * @return Iterable-Producto -- devuelve un iterable de productos.
+     */
     public Iterable<Producto> findAll();
 }
--- src/main/java/ctop/model/service/UsuarioServiceInterface.java
+++ src/main/java/ctop/model/service/UsuarioServiceInterface.java
@@ -2,8 +2,19 @@ package ctop.model.service;
 
 import ctop.model.entity.Usuario;
 
+/**
+ * Interfaz que define la implementacion que debe tener
+ * un servicio que maneja los usuarios.
+ * 
+ * @version 1.0
+ */
 public interface UsuarioServiceInterface {
 
+    /**
+     * Metodo que devuelve todos los usuarios disponibles en la
+     * base de datos.
+     * 
+     * @return Iterable-Usuario -- devuelve un iterable de productos.
+     */
     public Iterable<Usuario> findAll();
-    
 }
