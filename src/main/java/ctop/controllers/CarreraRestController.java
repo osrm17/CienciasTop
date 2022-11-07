b75cc00abebc395c112177cf2e7a1c0681fb572e
israncho <isragutierrez@ciencias.unam.mx>
2022-11-06 21:06:33 -0600

    lo faltante de carrera

---
+++ src/main/java/ctop/controllers/CarreraRestController.java
@@ -0,0 +1,28 @@
+package ctop.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ctop.model.entity.Carrera;
+import ctop.model.service.CarreraServiceInterface;
+
+/**
+ * Clase controlador que se encarga de manejar las peticiones con
+ * respecto a las carreras.
+ * 
+ * @version 1.0
+ */
+@RestController
+@RequestMapping("/api")
+public class CarreraRestController {
+
+    @Autowired
+    private CarreraServiceInterface carreraService;
+
+    @GetMapping("carreras")
+    public Iterable<Carrera> findAll() {
+        return carreraService.findAll();
+    }
+}
