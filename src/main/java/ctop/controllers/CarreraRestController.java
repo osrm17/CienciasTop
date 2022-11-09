03cebf12ae2bfe6741f13c44130f070870dcf0a0
israncho <isragutierrez@ciencias.unam.mx>
2022-11-08 19:42:51 -0600

    detalle

--- src/main/java/ctop/controllers/CarreraRestController.java
+++ src/main/java/ctop/controllers/CarreraRestController.java
@@ -1,6 +1,7 @@
 package ctop.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import ctop.model.service.CarreraServiceInterface;
  * 
  * @version 1.0
  */
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
 @RequestMapping("/api")
 public class CarreraRestController {
