4b759a559192de424bc2d36c5b2537247f92afb3
israncho <isragutierrez@ciencias.unam.mx>
2022-11-09 21:40:11 -0600

    ya se puede agregar a la bd

--- angular/src/app/productos/form.component.ts
+++ angular/src/app/productos/form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Producto } from './producto';
+import { ProductoService } from './producto.service';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  titulo: string = "Agregar producto"
+  producto: Producto = new Producto()
+
+  constructor(private productoService: ProductoService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  public create():void {
+    this.productoService.create(this.producto).subscribe(
+      response => this.router.navigate(['productos/'])
+    )
   }
 
 }
--- angular/src/app/productos/producto.service.ts
+++ angular/src/app/productos/producto.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Producto } from './producto';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaderResponse, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
 
-  private urlEndPoint : string = 'http://localhost:8080/api/productos'
+  private urlEndPoint : string = 'http://localhost:8080/api/productos';
+
+  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
 
   constructor(private http: HttpClient) { }
 
   getProductos(): Observable<Producto[]> {
     return this.http.get<Producto[]>(this.urlEndPoint);
+  }
+
+  create(producto: Producto):Observable<Producto> {
+    return this.http.post<Producto>(this.urlEndPoint, producto, {headers: this.httpHeaders});
   }
 }
