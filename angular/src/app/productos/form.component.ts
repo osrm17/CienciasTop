4f2bd7816d8659c6ce4452d38e32058ddc6b43b2
israncho <isragutierrez@ciencias.unam.mx>
2022-11-10 20:18:10 -0600

    alerta de creacion de producto

--- angular/src/app/productos/form.component.ts
+++ angular/src/app/productos/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Producto } from './producto';
 import { ProductoService } from './producto.service';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -18,9 +19,12 @@ export class FormComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public create():void {
-    this.productoService.create(this.producto).subscribe(
-      response => this.router.navigate(['productos/'])
+  public create(): void {
+    this.productoService.create(this.producto).subscribe(producto =>
+      {
+         this.router.navigate(['productos/'])
+         swal.fire('Nuevo Producto', `Producto ${producto.nombre} creado con éxito`, 'success')
+      }
     )
   }
 
