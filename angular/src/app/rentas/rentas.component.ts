e2ccaf4f60f3e7fa8f1e04fb9b7cb5f288ae2c52
osrm17 <rg.oscar17@ciencias.unam.mx>
2022-12-14 07:20:17 -0600

    ale y yo :3 metodo rentar de rentas.component.ts

--- angular/src/app/rentas/rentas.component.ts
+++ angular/src/app/rentas/rentas.component.ts
@@ -13,60 +13,76 @@ import swal from 'sweetalert2';
 registerLocaleData(localeEs, 'es');
 
 @Component({
-    selector: 'app-rentas',
-    templateUrl: './rentas.component.html',
-    styleUrls: ['./rentas.component.css'],
-    providers: [ { provide: LOCALE_ID, useValue: 'es' } ]
-  })
-  export class RentasComponent implements OnInit {
-  
-    rentas: Renta[];
-    renta: Renta = new Renta();
-    usuario: Usuario = new Usuario();
-    existencia: Existencia = new Existencia();
-    fecha = formatDate(new Date(), 'yyyy-MM-dd', 'es');
-  
-    constructor(private rentaService: RentaService,
-                private existenciaService: ExistenciaService, 
-                private authService: AuthService,
-                private router: Router, 
-                private activateRoute: ActivatedRoute) {
-    }
-  
-    ngOnInit(): void {
+  selector: 'app-rentas',
+  templateUrl: './rentas.component.html',
+  styleUrls: ['./rentas.component.css'],
+  providers: [{ provide: LOCALE_ID, useValue: 'es' }]
+})
+export class RentasComponent implements OnInit {
 
-      this.usuario = this.authService.usuario;
+  rentas: Renta[];
+  renta: Renta = new Renta();
+  usuario: Usuario = new Usuario();
+  existencia: Existencia = new Existencia();
+  fecha = formatDate(new Date(), 'yyyy-MM-dd', 'es');
 
-      this.rentaService.getRentas().subscribe(
-        rentas => this.rentas = rentas
-      );
+  constructor(private rentaService: RentaService,
+    private existenciaService: ExistenciaService,
+    private authService: AuthService,
+    private router: Router,
+    private activateRoute: ActivatedRoute) {
+  }
 
-      this.rentar();
-    }
+  ngOnInit(): void {
 
-    rentar(): void {
-      this.activateRoute.params.subscribe(params => {
-        let id = params['id']
-        if (id) {
+    this.usuario = this.authService.usuario;
 
-          this.existenciaService.getExistencia(id).subscribe((existencia)=> this.existencia = existencia)
+    this.rentaService.getRentas().subscribe(
+      rentas => this.rentas = rentas
+    );
 
+    //this.rentar();
+  }
+
+  rentar(): void {
+    this.activateRoute.params.subscribe(params => {
+      let id = params['id']
+      if (id) {
+
+        // obtener la existencia que tiene el numero de id 
+        this.existenciaService.getExistencia(id).subscribe((existencia) => {
+
+          this.existencia = existencia
+
+          // crear mi registro renta
           this.renta.idExistencia = id;
           this.renta.fechaRenta = this.fecha;
           this.renta.numct = this.usuario.numct;
-          this.renta.fechaDevolucion = "";
+          this.renta.fechaDevolucion = null;
 
-          this.rentaService.create(this.renta).subscribe(renta => this.renta = renta)
-
+          // me subscribo a la creacion del registro renta
+          this.rentaService.create(this.renta).subscribe(() => {
             this.existencia.estaRentado = true;
+            this.existenciaService.update(existencia).subscribe(() => {
+              this.router.navigate(['../historial']);
+              swal.fire('Producto rentado', `${this.usuario.nombre} rentaste ${this.existencia.codigo}`, 'success');
+            });
+          });
 
-            this.existenciaService.update(this.existencia).subscribe()
-
-            this.router.navigate(['../historial']);
-            swal.fire('Producto rentado', `${this.usuario.nombre} rentaste ${this.existencia.codigo}`, 'success');
-        
-        }
-      })
-    }
-  
+        });
+      }
+    });
   }
+
+}
+
+
+  //this.rentaService.create(this.renta).subscribe(renta => this.renta = renta)
+          //this.rentaService.create(this.renta).subscribe()
+
+            // this.existencia.estaRentado = true;
+
+            // this.existenciaService.update(this.existencia).subscribe()
+
+            // this.router.navigate(['../historial']);
+            // swal.fire('Producto rentado', `${this.usuario.nombre} rentaste ${this.existencia.codigo}`, 'success');
