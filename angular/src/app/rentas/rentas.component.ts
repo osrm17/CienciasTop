197a1e2c13a769c2320275dba924915da750fed1
osrm17 <rg.oscar17@ciencias.unam.mx>
2022-12-14 10:45:27 -0600

    Ya resta puntos al rentar

--- angular/src/app/rentas/rentas.component.ts
+++ angular/src/app/rentas/rentas.component.ts
@@ -9,6 +9,9 @@ import { RentaService } from './renta.service';
 import { formatDate, registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
 import swal from 'sweetalert2';
+import { ProductoService } from '../productos/producto.service';
+import { Producto } from '../productos/producto';
+import { UsuarioService } from '../usuarios/usuario.service';
 
 registerLocaleData(localeEs, 'es');
 
@@ -24,10 +27,13 @@ export class RentasComponent implements OnInit {
   renta: Renta = new Renta();
   usuario: Usuario = new Usuario();
   existencia: Existencia = new Existencia();
+  producto: Producto = new Producto();
   fecha = formatDate(new Date(), 'yyyy-MM-dd', 'es');
 
   constructor(private rentaService: RentaService,
     private existenciaService: ExistenciaService,
+    private productoService: ProductoService,
+    private usuarioService: UsuarioService,
     private authService: AuthService,
     private router: Router,
     private activateRoute: ActivatedRoute) {
@@ -54,6 +60,12 @@ export class RentasComponent implements OnInit {
 
           this.existencia = existencia
 
+          // var p: Producto;
+          // this.productoService.getProducto(existencia.codigo).subscribe((producto) => p = producto);
+
+          // this.usuario.pumaPuntos = this.usuario.pumaPuntos - p.costoPuntos;
+          // this.usuarioService.update(this.usuario).subscribe();
+
           // crear mi registro renta
           this.renta.idExistencia = id;
           this.renta.fechaRenta = this.fecha;
@@ -61,14 +73,26 @@ export class RentasComponent implements OnInit {
           this.renta.fechaDevolucion = null;
 
           // me subscribo a la creacion del registro renta
-          this.rentaService.create(this.renta).subscribe(() => {
+          this.rentaService.create(this.renta).subscribe(() =>
+          {
             this.existencia.estaRentado = true;
-            this.existenciaService.update(existencia).subscribe(() => {
+            this.existenciaService.update(existencia).subscribe(() =>
+            {
               this.router.navigate(['historial']);
               swal.fire('Producto rentado', `${this.usuario.nombre} rentaste ${this.existencia.codigo}`, 'success');
+              this.productoService.getProducto(existencia.codigo).subscribe((producto) =>
+              {
+                this.producto = producto;
+                var usr: Usuario = new Usuario();
+                this.usuarioService.getUsuario(this.usuario.numct).subscribe((usuario) =>
+                {
+                  usr = usuario;
+                  usr.pumaPuntos = usr.pumaPuntos - producto.costoPuntos;
+                  this.usuarioService.update(usr).subscribe();
+                });
+              });
             });
           });
-
         });
       }
     });
